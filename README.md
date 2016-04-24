f46a00b1997500f460a0646555b2474679b65100
Fritz Lin <uxfritz@163.com>
2016-04-24 14:05:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# 51voa-cli
+
+<a href="https://github.com/fritx/51voa-cli"><img width="90" height="20" src="https://fritx.github.io/51voa-cli/img/prs-welcome.svg" /></a>
+
+Downloading mp3 from <http://www.51voa.com/VOA_Standard_1.html>, etc
+
+```plain
+$ npm install -g 51voa-cli
+$ 51voa ./mp3 http://www.51voa.com/VOA_Standard_1.html
+```
+
+<img width="289" height="218" src="https://fritx.github.io/51voa-cli/img/preview.png" />
---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+'use strict'
+const request = require('request')
+const cheerio = require('cheerio')
+const mkdirp = require('mkdirp')
+const co = require('co')
+const fs = require('fs')
+const join = require('path').join
+const dirname = require('path').dirname
+const resolve = require('url').resolve
+
+const dir = process.argv[2] || process.cwd()
+
+// eg. VOA《常速英语 Standard English》听力下载 - 美国之音
+const listUrl = process.argv[3] || 'http://www.51voa.com/VOA_Standard_1.html'
+
+co(run)
+  .catch((err) => {
+    console.error(err.stack)
+  })
+
+function *run () {
+  console.log(`fetching... ${listUrl}`)
+  const html = yield fetch(listUrl)
+
+  const links = []
+  const $ = cheerio.load(html)
+  const $lis = $('#list a')
+  $lis.each((i, el) => {
+    const title = $(el).text()
+    const url = $(el).attr('href')
+    links.push({ title, url })
+  })
+  const len = links.length
+  console.log(`${len} links fetched`)
+
+  for (let i = 0; i < len; i++) {
+    const link = links[i]
+    // console.log(`fetching... ${i}/${len} - ${link.title}`)
+    let detailUrl = resolve(listUrl, link.url)
+    try {
+      const html = yield fetch(detailUrl)
+
+      // eg. Player("/201604/fusion-reactor-still-in-works.mp3");
+      const audioPath = html.match(/Player\("(.+?)"\);/)[1]
+        .substr(1) // removing leading `/`
+      const dest = join(dir, audioPath)
+
+      // eg. http://downdb.51voa.com/201604/fusion-reactor-still-in-works.mp3
+      const audioUrl = `http://downdb.51voa.com/${audioPath}`
+      console.log(`downloading... ${i}/${len} - ${link.title}`)
+      yield download(audioUrl, dest)  
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  console.log(`${len} files saved`)
+}
+
+function *download (url, dest) {
+  return yield (done) => {
+    if (fs.existsSync(dest)) return done() // skipping
+    mkdirp.sync(dirname(dest))
+    const writer = fs.createWriteStream(dest)
+    writer.on('error', (err) => { done(err) })
+    writer.on('finish', () => { done() })
+    request(url).pipe(writer)
+  }
+}
+
+function *fetch (url) {
+  return yield (done) => {
+    request(url, (err, res, html) => {
+      done(err, html)
+    })
+  }
+}
